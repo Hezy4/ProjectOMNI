ca922a783fe04d293fab01fc092b690e421f312d
Henry Boes <98630645+Hezy4@users.noreply.github.com>
2025-06-24 09:45:08 -0700

    Ready for Public Release

--- extension/background.js
+++
@@ -1,6 +0,0 @@
-//Henry Boes - Lyons Project - Internal Use Only.
-
-// extension/background.js
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Lyons Product Collector installed");
-});
